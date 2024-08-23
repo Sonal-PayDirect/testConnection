5af48a479653e6f0932ed65604c3f445cb8e8c88
Sonal Dhomne <sonaldhomne@productstack.com>
2024-08-23 19:26:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, null, null, {
+    host: process.env.DB_HOST,
+    dialect: 'mssql',
+    dialectOptions: {
+      options: {
+        encrypt: true,
+        trustServerCertificate: true
+      },
+      authentication: {
+        type: 'ntlm',
+        options: {
+          domain: process.env.DB_DOMAIN,
+          userName: process.env.DB_USER,
+          password: process.env.DB_PASSWORD
+        }
+      }
+    }
+  });  
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  } finally {
+    await sequelize.close();
+  }
+}
+
+testConnection();
